2a6458943cc3aee4bf289af3f19a1f6fcdbfc84b
khamali1337 <khamali1337@yandex.ru>
2021-10-08 00:16:52 -0400

    if branches

--- lovebirdCentos7.sh
+++ lovebirdCentos7.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #File created Oct 7, 2021
 #Indonesia
-#Ondi
+#khamali
 #
+#
+#Global var
+lnx=0
+lfdef=0
+lnosuid=0
+lnoexec=0
+lnodev=0
+buff=""
 #
 echo ""
 echo "LOVEBIRD CENTOS7 | Hardening Automation v1.0"
@@ -13,12 +21,24 @@ echo "Getting File System configuration from /etc/fstab"
 #cat /etc/fstab|grep /tmp
 #cat /etc/fstab|grep /var
 
-fs_tmp=`cat /etc/fstab|grep /tmp`
-#if [${#fs_tmp} == 0 ]; then echo "$fs_tmp not found"
-len_tmp=${#fs_tmp}
-#res=""
-if [ $len_tmp -ge 1 ];then
-	echo "Found:"
-	echo "$fs_tmp"
+fs_var=`cat /etc/fstab|grep /var`
+
+len_var=${#fs_var}
+if [ $len_var -ge 1 ];then
+	#echo "$fs_var"
+	fdef=`echo $fs_var|grep defaults`
+	lfdef=${#fdef}
+	if [ $lfdef -ge 1 ];then
+		buff=$fdef
+		nosuid=`echo $buff|grep nosuid` 
+		lnosuid=${#nosuid}
+		if [ $lnosuid -eq 0 ];then
+			buff=`echo $buff|sed 's/defaults/defaults,nosuid/g'`
+		fi
+		noexec=`echo $buff|grep noexec`
+		nodev=`echo $buff|grep nodev`
+		echo $buff
+	fi
+	
 fi
 #echo $res
